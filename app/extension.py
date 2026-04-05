9985d9dc0ae74493e5e26bc87f8694435b337620
Fenxozepxm2 <rip2046@mail.ru>
2026-04-06 02:43:47 +0700

    Добавил каркас БД, во ВТ буду тестировать её и пробовать связывать внешние сервисы

--- app/extension.py
+++ app/extension.py
@@ -2,10 +2,13 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 import logging
 import time
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from logging.handlers import RotatingFileHandler
 from flask_marshmallow import Marshmallow
 
-
+db = SQLAlchemy()
+migrate = Migrate()
 bcrypt = Bcrypt()
 cors = CORS()
 ma = Marshmallow()
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import create_app
+from app.models import db
 
 app = create_app()
 
@@ -9,4 +10,6 @@ def ping():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    with app.app_context():
+        db.create_all()
     
